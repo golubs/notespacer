9c45e9a692d64cc6cc116aafe8c64c28418857d8
golubs <oleggolubs@gmail.com>
2020-01-06 18:44:34 +0100

    Add logo icon

---
+++ client/src/components/Logo/LogoIcon.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Color = styled.svg`
+  fill: ${props => props.theme.colors.primary};
+`;
+
+const LogoIcon = () => {
+  return (
+    <Color
+      xmlns="http://www.w3.org/2000/svg"
+      width="32.725"
+      height="32.725"
+      viewBox="0 0 32.725 32.725"
+    >
+      <g transform="translate(-51 -219)">
+        <g transform="translate(51 219)">
+          <rect
+            width="7.126"
+            height="7.126"
+            rx="2"
+            transform="translate(0 5.506)"
+            fill="#fff"
+          ></rect>
+          <path
+            d="M2,0H5.126a2,2,0,0,1,2,2V5.126a2,2,0,0,1-2,2H2a2,2,0,0,1-2-2V2A2,2,0,0,1,2,0Z"
+            transform="matrix(0.883, 0.469, -0.469, 0.883, 15.636, 1.917)"
+            fill="#fff"
+          ></path>
+          <rect
+            width="7.126"
+            height="7.126"
+            rx="2"
+            transform="matrix(1, 0.017, -0.017, 1, 25.6, 0)"
+            fill="#fff"
+          ></rect>
+          <rect
+            width="7.126"
+            height="7.126"
+            rx="2"
+            transform="translate(0 15.552)"
+            fill="#fff"
+          ></rect>
+          <rect
+            width="7.126"
+            height="7.126"
+            rx="2"
+            transform="translate(10.047 15.552)"
+            fill="#fff"
+          ></rect>
+          <rect
+            width="7.126"
+            height="7.126"
+            rx="2"
+            transform="matrix(0.438, 0.899, -0.899, 0.438, 28.797, 11.24)"
+            fill="#fff"
+          ></rect>
+          <rect
+            width="7.126"
+            height="7.126"
+            rx="2"
+            transform="translate(0 25.599)"
+            fill="#fff"
+          ></rect>
+          <rect
+            width="7.126"
+            height="7.126"
+            rx="2"
+            transform="translate(10.047 25.599)"
+            fill="#fff"
+          ></rect>
+          <rect
+            width="7.126"
+            height="7.126"
+            rx="2"
+            transform="translate(20.094 25.599)"
+            fill="#fff"
+          ></rect>
+        </g>
+      </g>
+    </Color>
+  );
+};
+
+export default LogoIcon;
---
+++ client/src/stories/Logo.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LogoIcon from '../components/Logo/LogoIcon';
+
+export default {
+  title: 'Logo'
+};
+
+export function IconLogo() {
+  return <LogoIcon />;
+}
