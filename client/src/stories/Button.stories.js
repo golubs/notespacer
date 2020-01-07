2e13466494fbf88142032f5fa1925069ce793185
golubs <oleggolubs@gmail.com>
2020-01-07 02:21:27 +0100

    Add square button

---
+++ client/src/stories/Button.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SquareButton from '../components/Buttons/SquareButton';
+
+export default {
+  title: 'Buttons'
+};
+
+export function SquareButtonBase() {
+  return <SquareButton>Button</SquareButton>;
+}
