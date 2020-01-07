15c824f7cbf0a39194a0fc9b3fd5af0312868e00
golubs <32750555+golubs@users.noreply.github.com>
2020-01-08 00:09:07 +0100

    Add square buttons (#31)
    #25 
    
    * Add secondary font
    
    * Add square button
    
    * Add inverted square button

---
+++ client/src/components/Buttons/SquareButton.js
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const SquareButton = styled.button`
+  background: ${props => props.theme.colors.primary};
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 12px;
+  text-align: center;
+  color: ${props => props.theme.colors.fontsecondary};
+  width: 295px;
+  height: 48px;
+  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.16);
+`;
+
+export default SquareButton;
--- client/src/stories/Button.stories.js
+++ client/src/stories/Button.stories.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import SquareButton from '../components/Buttons/SquareButton';
+import SquareButtonInverted from '../components/Buttons/SquareButtonInverted';
 import AddNewNoteButton from '../components/Buttons/AddNewNoteButton';
 import StartRepetitionButton from '../components/Buttons/StartRepetitionButton';
 
 export default {
   title: 'Buttons'
 };
+
+export function SquareButtonBase() {
+  return <SquareButton>Button</SquareButton>;
+}
+
+export function InvertedSquareButton() {
+  return <SquareButtonInverted>Button</SquareButtonInverted>;
+}
 
 export function AddNewNote() {
   return <AddNewNoteButton />;
