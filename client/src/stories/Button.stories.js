957c3740f07ddb8c7f2c75e8bce7d93c2af9e741
golubs <oleggolubs@gmail.com>
2020-01-07 21:14:55 +0100

    Add button for adding new notes

---
+++ client/src/stories/Button.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AddNewNoteButton from '../components/Buttons/AddNewNoteButton';
+
+export default {
+  title: 'Buttons'
+};
+
+export function AddNewNote() {
+  return <AddNewNoteButton />;
+}
