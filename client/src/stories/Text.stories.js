a9dfdb4df818ee4e3a22c9d16f909d40054ca1c9
golubs <oleggolubs@gmail.com>
2020-01-08 12:38:24 +0100

    Add secondary all-caps text

--- client/src/stories/Text.stories.js
+++ client/src/stories/Text.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TextAllCapsPrimary from '../components/Text/TextAllCapsPrimary';
+import TextAllCapsSecondary from '../components/Text/TextAllCapsSecondary';
 
 export default {
   title: 'Text'
@@ -9,3 +10,6 @@ export function AllCapsPrimary() {
   return <TextAllCapsPrimary>TEXT</TextAllCapsPrimary>;
 }
 
+export function AllCapsSecondary() {
+  return <TextAllCapsSecondary>TEXT</TextAllCapsSecondary>;
+}
