0a58c8ad4f0a74bddecebcbf8ddcee6f5e0cb1a1
golubs <oleggolubs@gmail.com>
2020-01-08 12:37:36 +0100

    Add primary all-caps text

---
+++ client/src/stories/Text.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TextAllCapsPrimary from '../components/Text/TextAllCapsPrimary';
+
+export default {
+  title: 'Text'
+};
+
+export function AllCapsPrimary() {
+  return <TextAllCapsPrimary>TEXT</TextAllCapsPrimary>;
+}
+
