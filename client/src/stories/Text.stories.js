6d56d9f4c4c75232b5b46e8627bcb7d4ab36f99d
golubs <oleggolubs@gmail.com>
2020-01-08 14:27:23 +0100

    Add secondary intro text

--- client/src/stories/Text.stories.js
+++ client/src/stories/Text.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TextAllCapsPrimary from '../components/Text/TextAllCapsPrimary';
 import TextAllCapsSecondary from '../components/Text/TextAllCapsSecondary';
+import TextIntroSecondary from '../components/Text/TextIntroSecondary';
 
 export default {
   title: 'Text'
@@ -12,4 +13,15 @@ export function AllCapsPrimary() {
 
 export function AllCapsSecondary() {
   return <TextAllCapsSecondary>TEXT</TextAllCapsSecondary>;
+}
+
+export function IntroTextSecondary() {
+  return (
+    <TextIntroSecondary>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tempora,
+      similique quos explicabo doloremque aut deleniti aliquid blanditiis
+      voluptate quia excepturi laborum assumenda expedita quae eius libero hic
+      vitae sit.
+    </TextIntroSecondary>
+  );
 }
