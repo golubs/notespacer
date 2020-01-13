360dfaa769e3ca7085632d35823e2f9668ad1862
Oleg <32750555+golubs@users.noreply.github.com>
2020-01-13 13:13:25 +0100

    Prepare React Router (#39)

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import GlobalStyles from './GlobalStyles';
 import { ThemeProvider } from 'emotion-theming';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
 import light from './themes/light';
 import Intro from './user/pages/Intro';
+import NewNote from './notes/pages/NewNote';
 
 function App() {
   return (
     <ThemeProvider theme={light}>
       <GlobalStyles />
-      <Intro />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Intro />
+          </Route>
+          <Route path="/notes/new" exact>
+            <NewNote />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
