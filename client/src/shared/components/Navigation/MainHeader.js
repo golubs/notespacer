1dfdbdd28089a9ef4cd5d21aab70f77cee8f9975
golubs <oleggolubs@gmail.com>
2020-01-13 22:58:01 +0100

    Add main header

---
+++ client/src/shared/components/Navigation/MainHeader.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+
+const Header = styled.header`
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: #ee6665;
+  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.16);
+  padding: 0 1rem;
+  z-index: 5;
+`;
+
+const MainHeader = props => <Header>{props.children}</Header>;
+
+MainHeader.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default MainHeader;
---
+++ client/src/shared/components/Navigation/NavButton.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const NavButton = styled.button`
+  background: none;
+  border: none;
+  outline-style: none;
+  font-family: Arial;
+  font-weight: bold;
+  font-size: 25px;
+  text-align: center;
+  color: ${props => props.theme.colors.fontsecondary};
+  text-shadow: 0px 6px 6px rgba(0, 0, 0, 0.16);
+`;
+
+export default NavButton;
