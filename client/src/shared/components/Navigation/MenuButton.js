afa182a4cab2c63bdb9fa12f619d1b428fa4a4e5
golubs <oleggolubs@gmail.com>
2020-01-13 23:08:20 +0100

    Add main header

---
+++ client/src/shared/components/Navigation/MenuButton.js
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const MenuButton = styled.button`
+  width: 3rem;
+  height: 3rem;
+  background: transparent;
+  border: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  margin-right: 2rem;
+  cursor: pointer;
+  outline-style: none;
+`;
+
+export default MenuButton;
