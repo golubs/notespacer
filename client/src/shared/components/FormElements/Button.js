b047b3d403e50d7eb057383277915eabf4810302
Oleg <32750555+golubs@users.noreply.github.com>
2020-01-15 22:50:32 +0100

    Add button styles (#45)

---
+++ client/src/shared/components/FormElements/Button.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+
+const ButtonStyled = styled.button`
+  padding: 0.3rem 1rem;
+  border: 1px solid ${props => props.theme.colors.secondary};
+  border-radius: 2px;
+  background: transparent;
+  margin-right: 0.5rem;
+  font-size: 0.8rem;
+  color: ${props => props.theme.colors.fontsecondary};
+  display: inline-block;
+  outline: none;
+  &:hover {
+    border: 1px solid ${props => props.theme.colors.fonttertiary};
+    background: ${props => props.theme.colors.secondary};
+  }
+  &:active {
+    border: 1px solid ${props => props.theme.colors.fonttertiary};
+    background: ${props => props.theme.colors.fonttertiary};
+  }
+`;
+
+const Button = props => <ButtonStyled>{props.children}</ButtonStyled>;
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Button;
