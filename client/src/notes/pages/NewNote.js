50f8ea727376ffe31707798d34cd47d6508115dd
golubs <oleggolubs@gmail.com>
2020-01-16 10:54:00 +0100

    Add new note form

--- client/src/notes/pages/NewNote.js
+++ client/src/notes/pages/NewNote.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import MainNavigation from '../../shared/components/Navigation/MainNavigation';
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
+
+import Input from '../../shared/components/FormElements/Input';
+import ArrowLeftPrimary from '../../shared/components/Icons/ArrowLeftPrimary';
+import PlusPrimary from '../../shared/components/Icons/PlusPrimary';
+import Nav from '../../shared/components/Layout/Nav';
+
+const Form = styled.form`
+  position: relative;
+  list-style: none;
+  width: 90%;
+  max-width: 40rem;
+  margin: 0 auto;
+  border: 2px solid #c1c1c1;
+  box-shadow: 0 2px 8px rgb(0, 0, 0, 0, 26);
+  border-radius: 1px;
+  padding: 1rem;
+  overflow: hidden;
+  background: ${props => props.theme.colors.background};
+`;
 
 const NewNote = () => {
   return (
     <>
-      <MainNavigation />
-      <h2>New Note</h2>
+      <Nav>
+        <NavLink to="/notes">
+          <ArrowLeftPrimary />
+        </NavLink>
+        <PlusPrimary />
+      </Nav>
+      <Form>
+        <Input id="title" element="input" type="text" label="Title" />
+        <Input id="note" element="textarea" type="text" label="Note" />
+      </Form>
     </>
   );
 };
---
+++ client/src/shared/components/Layout/Nav.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+const Nav = styled.nav`
+  max-width: 40rem;
+  padding: 2rem 1.2rem 1.5rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export default Nav;
